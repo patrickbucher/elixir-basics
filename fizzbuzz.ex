85d52a78ec0faee4c4d7f3cea8fede0c44f0dc6a
Patrick Bucher <patrick.bucher@mailbox.org>
2022-02-16 07:39:36 +0100

    FizzBuzz examples (inspired by chapter 2 of "Phoenix in Action")

---
+++ fizzbuzz.ex
@@ -0,0 +1,57 @@
+defmodule FizzBuzz do
+  defmodule UnlessIfElse do
+    def fizzbuzz(min, max), do: Enum.each(min..max, &fizzbuzz/1)
+
+    defp fizzbuzz(x) do
+      unless rem(x, 3) == 0 or rem(x, 5) == 0 do
+        IO.puts(x)
+      end
+
+      if rem(x, 15) == 0 do
+        IO.puts("FizzBuzz")
+      else
+        if rem(x, 3) == 0 do
+          IO.puts("Fizz")
+        else
+          if rem(x, 5) == 0 do
+            IO.puts("Buzz")
+          end
+        end
+      end
+    end
+  end
+
+  defmodule Multiclause do
+    def fizzbuzz(min, max), do: Enum.each(min..max, &fizzbuzz/1)
+    defp fizzbuzz(x) when rem(x, 15) == 0, do: IO.puts("FizzBuzz")
+    defp fizzbuzz(x) when rem(x, 3) == 0, do: IO.puts("Fizz")
+    defp fizzbuzz(x) when rem(x, 5) == 0, do: IO.puts("Buzz")
+    defp fizzbuzz(x), do: IO.puts(x)
+  end
+
+  defmodule Cond do
+    def fizzbuzz(min, max), do: Enum.each(min..max, &fizzbuzz/1)
+
+    defp fizzbuzz(x) do
+      cond do
+        rem(x, 15) == 0 -> IO.puts("FizzBuzz")
+        rem(x, 3) == 0 -> IO.puts("Fizz")
+        rem(x, 5) == 0 -> IO.puts("Buzz")
+        true -> IO.puts(x)
+      end
+    end
+  end
+
+  defmodule Case do
+    def fizzbuzz(min, max), do: Enum.each(min..max, &fizzbuzz/1)
+
+    defp fizzbuzz(x) do
+      case {rem(x, 3), rem(x, 5)} do
+        {0, 0} -> IO.puts("FizzBuzz")
+        {0, _} -> IO.puts("Fizz")
+        {_, 0} -> IO.puts("Buzz")
+        {_, _} -> IO.puts(x)
+      end
+    end
+  end
+end
