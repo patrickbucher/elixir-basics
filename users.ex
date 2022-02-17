af13068043d9e76efe6ebaf567d0dff021a1c80b
Patrick Bucher <patrick.bucher@mailbox.org>
2022-02-17 07:45:40 +0100

    created example data for upcoming with example

---
+++ users.ex
@@ -0,0 +1,45 @@
+people = [
+  %{
+    name: "Dilbert",
+    username: "dilbo",
+    password: "Uyee7oox0OK8johG",
+    email: "dilbo@corp.com",
+    age: 42
+  },
+  %{
+    name: "Pointy Haired Boss",
+    email: "boss@corp.com",
+    age: 52,
+    golf_handicap: 17,
+    cars_owned: 3
+  },
+  %{
+    name: "Wally",
+    username: "lazybone",
+    password: "qwerty",
+    email: "wally@corp.com",
+    age: 47,
+    years_wasted: 27
+  },
+  %{
+    name: "Dogbert",
+    email: "doggo@corp.com",
+    age: 13,
+    current_lawsuits: 3,
+    allegations: ["fraud", "arson", "tax evasion"]
+  },
+  %{
+    name: "Alice",
+    username: "alicepro",
+    password: "IHateThisPlace",
+    email: "alice@corp.com",
+    age: 39
+  },
+  %{
+    name: "Catbert",
+    email: "catto@corp.com",
+    age: 11,
+    years_in_jail: 5,
+    former_employers: ["aramco", "facebook"]
+  }
+]
