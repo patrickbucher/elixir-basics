92b340cf68c4caadacf4c1c72f7f5d02c7eabec8
Patrick Bucher <patrick.bucher@mailbox.org>
2022-03-07 20:35:15 +0100

    todo list/server: separation of concerns

--- examples/todo/todo_list_v8.exs
+++ examples/todo/todo_list_v8.exs
@@ -1,13 +1,49 @@
 defmodule TodoList do
   defstruct auto_id: 1, entries: %{}
+
+  def new() do
+    %TodoList{}
+  end
+
+  def add_entry(todo_list, entry) do
+    entry = Map.put(entry, :id, todo_list.auto_id)
+    new_entries = Map.put(todo_list.entries, todo_list.auto_id, entry)
+    %TodoList{todo_list | entries: new_entries, auto_id: todo_list.auto_id + 1}
+  end
+
+  def update_entry(todo_list, entry_id, updater_fun) do
+    case Map.fetch(todo_list.entries, entry_id) do
+      :error ->
+        todo_list
+
+      {:ok, old_entry} ->
+        new_entry = %{id: ^entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(todo_list.entries, entry_id, new_entry)
+        %TodoList{todo_list | entries: new_entries}
+    end
+  end
+
+  def delete_entry(todo_list, entry_id) do
+    new_entries = Map.filter(todo_list.entries, fn {_, e} -> e.id != entry_id end)
+    %TodoList{todo_list | entries: new_entries}
+  end
+
+  def entries(todo_list, date) do
+    todo_list.entries
+    |> Stream.filter(fn {_, entry} -> entry.date == date end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+end
+
+defmodule TodoServer do
   use GenServer
 
   def start() do
-    GenServer.start(TodoList, nil)
+    GenServer.start(TodoServer, nil)
   end
 
-  def entries(pid, date) do
-    GenServer.call(pid, {:get, date})
+  def add_entry(pid, entry) do
+    GenServer.cast(pid, {:add, entry})
   end
 
   def update_entry(pid, entry_id, updater_fun) do
@@ -18,68 +54,45 @@ defmodule TodoList do
     GenServer.cast(pid, {:del, entry_id})
   end
 
-  def add_entry(pid, entry) do
-    GenServer.cast(pid, {:add, entry})
+  def entries(pid, date) do
+    GenServer.call(pid, {:get, date})
   end
 
   def init(_) do
-    {:ok, %TodoList{}}
+    {:ok, TodoList.new()}
   end
 
   def handle_cast({:add, entry}, state) do
-    entry = Map.put(entry, :id, state.auto_id)
-    new_entries = Map.put(state.entries, state.auto_id, entry)
-
-    new_state = %TodoList{
-      state
-      | entries: new_entries,
-        auto_id: state.auto_id + 1
-    }
-
-    {:noreply, new_state}
+    {:noreply, TodoList.add_entry(state, entry)}
   end
 
   def handle_cast({:upd, entry_id, updater_fun}, state) do
-    case Map.fetch(state.entries, entry_id) do
-      :error ->
-        {:noreply, state}
-
-      {:ok, old_entry} ->
-        new_entry = %{id: ^entry_id} = updater_fun.(old_entry)
-        new_entries = Map.put(state.entries, entry_id, new_entry)
-        {:noreply, %TodoList{state | entries: new_entries}}
-    end
+    {:noreply, TodoList.update_entry(state, entry_id, updater_fun)}
   end
 
   def handle_cast({:del, entry_id}, state) do
-    new_entries = Map.filter(state.entries, fn {_, e} -> e.id != entry_id end)
-    {:noreply, %TodoList{state | entries: new_entries}}
+    {:noreply, TodoList.delete_entry(state, entry_id)}
   end
 
   def handle_call({:get, date}, _, state) do
-    found_entries =
-      state.entries
-      |> Stream.filter(fn {_, entry} -> entry.date == date end)
-      |> Enum.map(fn {_, entry} -> entry end)
-
-    {:reply, found_entries, state}
+    {:reply, TodoList.entries(state, date), state}
   end
 end
 
-{:ok, pid} = TodoList.start()
+{:ok, pid} = TodoServer.start()
 
 IO.puts("adding some entries")
-TodoList.add_entry(pid, %{date: ~D[2022-02-26], title: "Study Elixir"})
-TodoList.add_entry(pid, %{date: ~D[2022-02-26], title: "Cleaning Up"})
-TodoList.add_entry(pid, %{date: ~D[2022-02-27], title: "Update Software"})
+TodoServer.add_entry(pid, %{date: ~D[2022-02-26], title: "Study Elixir"})
+TodoServer.add_entry(pid, %{date: ~D[2022-02-26], title: "Cleaning Up"})
+TodoServer.add_entry(pid, %{date: ~D[2022-02-27], title: "Update Software"})
 
 IO.puts("entries from 2022-02-26")
-Enum.each(TodoList.entries(pid, ~D[2022-02-26]), &IO.inspect/1)
+Enum.each(TodoServer.entries(pid, ~D[2022-02-26]), &IO.inspect/1)
 
 IO.puts("entries from 2022-02-26 after deleting entry with id 2")
-TodoList.delete_entry(pid, 2)
-Enum.each(TodoList.entries(pid, ~D[2022-02-26]), &IO.inspect/1)
+TodoServer.delete_entry(pid, 2)
+Enum.each(TodoServer.entries(pid, ~D[2022-02-26]), &IO.inspect/1)
 
 IO.puts("entries from 2022-02-27 after updating entry with id 3")
-TodoList.update_entry(pid, 3, fn e -> Map.put(e, :title, "Relaxing") end)
-Enum.each(TodoList.entries(pid, ~D[2022-02-27]), &IO.inspect/1)
+TodoServer.update_entry(pid, 3, fn e -> Map.put(e, :title, "Relaxing") end)
+Enum.each(TodoServer.entries(pid, ~D[2022-02-27]), &IO.inspect/1)
