8e9dca0a07890de0310f2cd04caa9b86eb297933
Patrick Bucher <patrick.bucher@mailbox.org>
2022-03-07 20:41:42 +0100

    buddies/buddies server: separation of concerns

--- examples/buddies/buddies_v8.exs
+++ examples/buddies/buddies_v8.exs
@@ -1,5 +1,41 @@
 defmodule Buddies do
   defstruct auto_id: 1, entries: %{}
+
+  def new() do
+    %Buddies{}
+  end
+
+  def add_entry(buddies, entry) do
+    entry = Map.put(entry, :id, buddies.auto_id)
+    new_entries = Map.put(buddies.entries, buddies.auto_id, entry)
+    %Buddies{buddies | entries: new_entries, auto_id: buddies.auto_id + 1}
+  end
+
+  def update_entry(buddies, entry_id, updater_fun) do
+    case Map.fetch(buddies.entries, entry_id) do
+      :error ->
+        buddies
+
+      {:ok, old_entry} ->
+        new_entry = %{id: ^entry_id} = updater_fun.(old_entry)
+        new_entries = Map.put(buddies.entries, entry_id, new_entry)
+        %Buddies{buddies | entries: new_entries}
+    end
+  end
+
+  def delete_entry(buddies, entry_id) do
+    new_entries = Map.filter(buddies.entries, fn {_, e} -> e.id != entry_id end)
+    %Buddies{buddies | entries: new_entries}
+  end
+
+  def entries(buddies, city) do
+    buddies.entries
+    |> Stream.filter(fn {_, entry} -> entry.city == city end)
+    |> Enum.map(fn {_, entry} -> entry end)
+  end
+end
+
+defmodule BuddyServer do
   use GenServer
 
   def start() do
@@ -23,63 +59,40 @@ defmodule Buddies do
   end
 
   def init(_) do
-    {:ok, %Buddies{}}
+    {:ok, Buddies.new()}
   end
 
   def handle_cast({:add, entry}, state) do
-    entry = Map.put(entry, :id, state.auto_id)
-    new_entries = Map.put(state.entries, state.auto_id, entry)
-
-    new_buddies = %Buddies{
-      state
-      | entries: new_entries,
-        auto_id: state.auto_id + 1
-    }
-
-    {:noreply, new_buddies}
+    {:noreply, Buddies.add_entry(state, entry)}
   end
 
   def handle_cast({:upd, entry_id, updater_fun}, state) do
-    case Map.fetch(state.entries, entry_id) do
-      :error ->
-        {:noreply, state}
-
-      {:ok, old_entry} ->
-        new_entry = %{id: ^entry_id} = updater_fun.(old_entry)
-        new_entries = Map.put(state.entries, entry_id, new_entry)
-        {:noreply, %Buddies{state | entries: new_entries}}
-    end
+    {:noreply, Buddies.update_entry(state, entry_id, updater_fun)}
   end
 
   def handle_cast({:del, entry_id}, state) do
-    new_entries = Map.filter(state.entries, fn {_, e} -> e.id != entry_id end)
-    {:noreply, %Buddies{state | entries: new_entries}}
+    {:noreply, Buddies.delete_entry(state, entry_id)}
   end
 
   def handle_call({:get, city}, _, state) do
-    found_entries =
-      state.entries
-      |> Stream.filter(fn {_, entry} -> entry.city == city end)
-      |> Enum.map(fn {_, entry} -> entry end)
-
-    {:reply, found_entries, state}
+    {:reply, Buddies.entries(state, city), state}
   end
 end
 
-{:ok, pid} = Buddies.start()
+{:ok, pid} = BuddyServer.start()
 
 IO.puts("adding some buddies")
-Buddies.add_entry(pid, %{city: "Berlin", name: "Hans"})
-Buddies.add_entry(pid, %{city: "Berlin", name: "Hermann"})
-Buddies.add_entry(pid, %{city: "Palermo", name: "Vito"})
+BuddyServer.add_entry(pid, %{city: "Berlin", name: "Hans"})
+BuddyServer.add_entry(pid, %{city: "Berlin", name: "Hermann"})
+BuddyServer.add_entry(pid, %{city: "Palermo", name: "Vito"})
 
 IO.puts("entries from Berlin")
-Enum.each(Buddies.entries(pid, "Berlin"), &IO.inspect/1)
+Enum.each(BuddyServer.entries(pid, "Berlin"), &IO.inspect/1)
 
 IO.puts("entries from Berlin after deleting entry with id 2")
-Buddies.delete_entry(pid, 2)
-Enum.each(Buddies.entries(pid, "Berlin"), &IO.inspect/1)
+BuddyServer.delete_entry(pid, 2)
+Enum.each(BuddyServer.entries(pid, "Berlin"), &IO.inspect/1)
 
 IO.puts("entries from Palermo after updating entry with id 3")
-Buddies.update_entry(pid, 3, fn e -> Map.put(e, :name, "Don Corleone") end)
-Enum.each(Buddies.entries(pid, "Palermo"), &IO.inspect/1)
+BuddyServer.update_entry(pid, 3, fn e -> Map.put(e, :name, "Don Corleone") end)
+Enum.each(BuddyServer.entries(pid, "Palermo"), &IO.inspect/1)
